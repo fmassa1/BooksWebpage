196bd4c658ce193e0af4fe84cdc6f3a0ceab86f5
Fredric Massa <rickmass2020@gmail.com>
2026-01-28 11:52:26 -0600

    added swagger to test apis

--- Program.cs
+++ Program.cs
@@ -18,14 +18,24 @@ builder.Services.AddScoped<IBookService, BookService>();
 
 builder.Services.AddControllers();
 
-
 builder.Services.AddRazorPages();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Books API v1");
+        options.RoutePrefix = "swagger";
+    });
+
     app.UseMigrationsEndPoint();
 }
 else
