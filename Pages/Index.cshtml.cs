8f5be5eb00af5bce6618783d48392c89ed201ea5
Fredric Massa <rickmass2020@gmail.com>
2026-01-28 10:33:18 -0600

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BooksWebpage.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
